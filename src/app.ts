c61042e8b1bf47bdca2697485b2d2ab91f136878
Anthony Igor <igor.torrestj11@gmail.com>
2024-08-29 22:02:19 -0300

    add: update docker-compose / add script create customers

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import applicationRoutes from './routes';
 import { errorHandling } from './middlewares/errorHandling';
 import 'dotenv/config';
+import { CustomerRepository } from './repositories/CustomerRepository';
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.use(express.static('public'));
 
 //routes
 app.use('/', applicationRoutes);
+
+// cria customers para testes das rotas
+const createCustomersTest = async () => {
+    const customerRepository = new CustomerRepository();
+    await customerRepository.createCustomer('9ff6186a-9ba3-42a4-a2a5-3a668dfe6b72', 'Jonh Doe');
+    await customerRepository.createCustomer('d3b981bd-ab76-40b6-be4a-cf8a6e195808', 'Maria D');
+}
+createCustomersTest();
 
 app.use(errorHandling)
 // initialize
--- src/repositories/CustomerRepository.ts
+++ src/repositories/CustomerRepository.ts
@@ -2,6 +2,17 @@ import { Customer } from "@prisma/client";
 import { prisma } from "../lib/prisma";
 
 export class CustomerRepository {
+    async createCustomer(id: string, name: string): Promise<Customer> {
+        const customer = await prisma.customer.create({
+            data: {
+                id,
+                name
+            }
+        });
+
+        return customer;
+    }
+
     async getCustomerByUUID(uuid: string): Promise<Customer | null> {
         const customer = await prisma.customer.findUnique({
             where: {
